03ca065fba8af336a8135e9a211b12da13248bbc
polikmm <polikmm@mail.ru>
2024-08-02 11:06:56 +0400

    feat: write solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = {};
+  bracketsConfig.forEach( arr => brackets[arr[0]] = arr[1])
+  let cache = [];
+  str = str.split('')
+  for (let i = 0; i < str.length; i++) {
+    if(!cache) cache.push(str[i])
+    if(cache) {
+      brackets[cache[cache.length - 1]] == str[i] ? cache.pop() : cache.push(str[i])
+    }
+  }
+  return cache.length === 0 
 }
